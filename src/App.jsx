47039e4a394798c0664cac5fc0fe72841fd3a51c
abdulsamiabbasi812 <samiabasi087@gmail.com>
2025-03-27 01:12:39 +0500

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -24,9 +24,6 @@ function App() {
   ])
   return (
     <>
-
-
-
       <RouterProvider router={router} />
     </>
   )
--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/vite-react-app",
-})
+  base: '/vite-react-app/',
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
+  },
+});
